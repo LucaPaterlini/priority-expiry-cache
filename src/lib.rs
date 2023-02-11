07e40551c9400f5b35927fca2c184854eb290906
Luca Paterlini <lucapaterlini@Lucas-MacBook-Pro.local>
2023-02-11 17:00:50 +0000

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+// This version its just to task we can publish a repo on crates.io
+// the v 0.2.0 will contain the first version of the code
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
